7e1784ffcf271872bfa7c06ce3211369b7a39742
Kyle Hensel <k.y.l.e@outlook.co.nz>
2021-10-13 15:30:15 +1300

    feat: use underscores to show hint

--- src/application/helpers/Format.java
+++ src/application/helpers/Format.java
@@ -13,4 +13,32 @@ public class Format {
 	public static String formatAsTime(int seconds) {
 		return String.format("%02d:%02d", (seconds % 3600) / 60, seconds % 60);
 	}
+
+	/**
+	 * converts a word into underscores for the hint, e.g. "Hello" -> "_ _ _ _ _"
+	 * If `includeSomeLetters` is true, then the result would be "H _ _ _ o"
+	 */
+	public static String getUnderscoreHint(
+		String word,
+		Boolean includeSomeLetters
+	) {
+		int lastIndex = word.length() - 1;
+
+		StringBuilder withUnderscore = new StringBuilder(
+			word
+				.replaceAll("[\\wāēīōūĀĒĪŌŪ]", "_") // replace (A-Z, a-z, and macrons) with underscores
+				.replaceAll(".", "$0 ") // then add a space between every character
+				.trim() // trim to remove the last space character
+		);
+
+		if (includeSomeLetters) {
+			// add back the first and last letter.
+			withUnderscore.setCharAt(0, word.charAt(0));
+			withUnderscore.setCharAt(lastIndex * 2, word.charAt(lastIndex));
+			// we use `lastIndex * 2` because withUnderscore is double as long as
+			// the original, since it has spaces between every letter.
+		}
+
+		return withUnderscore.toString();
+	}
 }
