ef79794ce8cc30ebf1b37bb628dfcec69504ceca
Kyle Hensel <k.y.l.e@outlook.co.nz>
2021-10-13 17:02:05 +1300

    chore: translate the help messages into te reo

--- src/application/helpers/Help.java
+++ src/application/helpers/Help.java
@@ -1,6 +1,7 @@
 package application.helpers;
 
 import javafx.scene.control.Alert;
+import javafx.scene.layout.Region;
 
 /**
  * a class which shows a popup with help/instructions.
@@ -13,17 +14,31 @@ public class Help {
 		GAME,
 	}
 
+	/**
+	 * gets the appropriate help message for the current page/category.
+	 *
+	 * The comments are the transliteral translation
+	 */
 	private static String getHelpMessage(Category category) {
 		switch (category) {
 			case TOPIC_SLECTION:
-				return "Click a topic. Then click Start to begin.";
+				return (
+					"Ka taea e koe te kōwhiri marau, muri iho pāwhiri te pātene tīmata." + // You can select a topic, afterwards press the start button.
+					"\n" +
+					"\nClick a topic. Then click Start to begin."
+				);
 			case GAME:
 				return (
-					"Type the word into the textbox to play. " +
+					"Patohia te kupu ki te pouaka kupu, ati pāwhiri te pātuhi tāuru." + // Type the word into the text box, then press the enter key.
+					"\nKi te whakarongo ano i te kupu korero, pāwhiri te 'toai' pātene." + // To hear the spoken word again, click the 'repeat' button.
+					"\nKi te kore koe e mohio, pāwhiri 'pahemo'." + // If you don't know, press 'skip'
+					"\nPāwhiri nga pātene tohutō, ki te kōkuhu i tohutō" + // Press the button [of the] macrons(pl), to insert macrons(pl).
+					"\n" +
+					"\nType the word into the textbox to play. " +
 					"\nClick enter or the submit button to test the word. " +
 					"\nThe repeat button reads out the word again. " +
-					"\nThe skip button moves onto the next word" +
-					"\nClick the macron buttons to add vowels with macrons"
+					"\nThe skip button moves onto the next word." +
+					"\nClick the macron buttons to add vowels with macrons."
 				);
 			default:
 				return ""; // will never happen
@@ -32,9 +47,10 @@ public class Help {
 
 	public static void showPopup(Category category) {
 		Alert instructions = new Alert(Alert.AlertType.INFORMATION);
-		instructions.setTitle("Āwhina / Instructions");
+		instructions.setTitle("Āwhina / Help");
 		instructions.setHeaderText(null);
 		instructions.setContentText(getHelpMessage(category));
+		instructions.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 		instructions.show();
 	}
 }
